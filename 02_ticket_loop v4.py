40d7b1ebd3ad83bc8e3ad74b289a1683bef1ad8b
httpscolonslashslashgithubdotcomslash <83557975+httpscolonslashslashgithubdotcomslash@users.noreply.github.com>
2022-03-16 20:07:52 +1300

    Component 2

---
+++ 02_ticket_loop v4.py
@@ -0,0 +1,19 @@
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "Xxx" and count < MAX_TICKETS:
+    if MAX_TICKETS - count > 1:
+        print(f"\nYou have {MAX_TICKETS - count} seats left.")
+    else:
+        print(f"\n**** You have ONLY ONE seat left! ****")
+    name = input("What's your name? ").title()
+    if name != "Xxx":
+        count += 1
+
+if count < MAX_TICKETS:
+    print(f"\nYou have sold {count} tickets\nThere are still"
+          f" {MAX_TICKETS - count} available")
+
+else:
+    print("\nYou have sold all the available tickets.")
